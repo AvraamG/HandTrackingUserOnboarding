fd33a8e1eca0977516ba9aafdd8c3174c9048cba
Avraam Georgiadis <georgiadis.ab@gmail.com>
2020-12-14 16:32:31 +0100

    Initial Commit with all the files.

---
+++ Assets/InteractableCubeInstractions.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class InteractableCubeInstractions : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+        ManomotionManager.OnManoMotionFrameProcessed += HandleManoMotionFrameProcessed;
+    }
+
+    #region Instruction Rules
+    //Set of rules that my Instructions will follow.
+    // 1. -----  Creative challenge  ---- The users should have the intention of interaction with this cube
+    // 2. The detecteed trigger is the one I am looking for (example Click). 
+    // 3. Steps (1)* - 2 should happen 3 times.
+    // 4. After step 3 is validated Hide the gesture Animator and info text.
+
+
+    [SerializeField]
+    const int maxInteractionTimes = 3;
+    [SerializeField]
+    ManoGestureTrigger interactionTrigger = ManoGestureTrigger.CLICK;
+    private int currentInteractionCounter = 0;
+
+    #endregion
+
+    #region Components
+    [SerializeField]
+    GameObject gestureAnimation;
+    [SerializeField]
+    GameObject infoText;
+
+    private MeshRenderer meshRenderer;
+    #endregion
+
+
+
+    void HandleManoMotionFrameProcessed()
+    {
+        HandInfo currentHandInfo = ManomotionManager.Instance.Hand_infos[0].hand_info;
+
+        //This means that the current trigger gesture 
+        if (currentHandInfo.gesture_info.mano_gesture_trigger == interactionTrigger)
+        {
+
+            if (currentInteractionCounter < maxInteractionTimes)
+            {
+                currentInteractionCounter++;
+
+            }
+            else
+            {
+                gestureAnimation.gameObject.SetActive(false);
+                infoText.gameObject.SetActive(false);
+                Handheld.Vibrate();
+            }
+
+
+            ChangeColor();
+        }
+
+    }
+
+    void ChangeColor()
+    {
+        Color newColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        meshRenderer.material.color = newColor;
+
+    }
+}
---
+++ Assets/UserOnboarding/InstructionsDetection.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsDetection : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        ManomotionManager.OnManoMotionFrameProcessed += HandleManoMotionFrameProcessed;
+    }
+
+    #region Instruction Rules
+    //Set of rules that my Instructions will follow.
+    // 1. The Hand is Detected.
+    // 2. The hand is not too close to the Screen.
+    // 3. Steps 1 and 2 should be consistent for 60 Frames.
+    // 4. Show the temporal 
+
+    [SerializeField]
+    const int maxGoodFrames = 60;
+    [SerializeField]
+    const float maxAcceptedBoundingBoxWidth = 0.9f;
+    private int currentGoodFrames = 0;
+
+    #endregion
+
+    #region Components
+    [SerializeField]
+    Image radialProgressImage;
+
+    #endregion
+
+    void HandleManoMotionFrameProcessed()
+    {
+        HandInfo currentHandInfo = ManomotionManager.Instance.Hand_infos[0].hand_info;
+
+        //This means that there is a Hand Detection
+        if (currentHandInfo.gesture_info.mano_class != ManoClass.NO_HAND)
+        {
+            //This means that the hand is not so close 
+            if (currentHandInfo.tracking_info.bounding_box.width <= maxAcceptedBoundingBoxWidth)
+            {
+                IncreaseGoodFrames();
+            }
+
+        }
+        else
+        {
+            //Decide if I want to Decrease Good frames and visually update the progress.
+        }
+
+        if (currentGoodFrames == maxGoodFrames)
+        {
+            ValidateSuccess();
+        }
+    }
+
+    private void IncreaseGoodFrames()
+    {
+        if (currentGoodFrames < maxGoodFrames)
+        {
+            currentGoodFrames++;
+            radialProgressImage.fillAmount = (float)currentGoodFrames / maxGoodFrames;
+        }
+
+    }
+
+    private void DecreaseGoodFrames()
+    {
+        if (currentGoodFrames > 0)
+        {
+            currentGoodFrames--;
+            radialProgressImage.fillAmount = (float)currentGoodFrames / maxGoodFrames;
+        }
+
+    }
+
+    private void ValidateSuccess()
+    {
+        Handheld.Vibrate();
+        ManomotionManager.OnManoMotionFrameProcessed -= HandleManoMotionFrameProcessed;
+        this.transform.gameObject.SetActive(false);
+
+    }
+
+
+}
